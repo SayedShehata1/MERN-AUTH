957175d241a538c9107205349d2d50410453d404
Sayed <sayedshehata33@gmail.com>
2025-03-12 01:34:01 +0200

    feat: :wrench: Initialize React app with Vite, add assets, and configure Tailwind and other packages

---
+++ Client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className="text-4xl text-center">
+            <h1>React App</h1>
+        </div>
+    );
+};
+
+export default App;
