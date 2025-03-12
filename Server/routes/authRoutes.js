28f470320a9b10ecaa64a413292275674784a7d0
Sayed <sayedshehata33@gmail.com>
2025-03-12 05:19:00 +0200

    feat: :passport_control: Add AppContext for authentication state management and integrate Toast notifications

--- Server/routes/authRoutes.js
+++ Server/routes/authRoutes.js
@@ -19,7 +19,7 @@ authRouter.post('/login', login);
 authRouter.post('/logout', logout);
 authRouter.post('/send-verify-otp', userAuth, sendVerifyOtp); // will call first the middleware (userAuth) send the userId in the req then call the controller sendVerifyOtp
 authRouter.post('/verify-account', userAuth, verifyEmail);
-authRouter.post('/is-auth', userAuth, isAuthenticated); // check if the user is authenticated
+authRouter.get('/is-auth', userAuth, isAuthenticated); // check if the user is authenticated
 authRouter.post('/send-reset-otp', sendResetOtp); // send the reset otp to the user to reset the password
 authRouter.post('/reset-password', resetPassword); // reset the password
 
